33e336770cecd265b3f37dd22572538324078777
itokeso <61177741+itokeso@users.noreply.github.com>
2020-04-21 23:12:43 +0900

    drill

---
+++ drill17.rb
@@ -0,0 +1,11 @@
+# 以下のハッシュに対してメソッドを使用し、キーだけを1つの配列に入れてください。
+
+attr = {name: "田中", age: 27, height: 180, weight: 75}
+
+array = attr.keys
+array1 = attr.values
+array2 = attr.to_a
+
+puts array
+puts array1
+puts array2
