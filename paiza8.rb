6f2982aad6627dc0f9ad917fcdaf11c1ed223af5
itokeso <61177741+itokeso@users.noreply.github.com>
2020-06-05 21:01:06 +0900

    paiza

--- paiza8.rb
+++ paiza8.rb
@@ -1,20 +1,13 @@
 # D ある正の整数aとbがスペース区切りで入力されます。
 
-# aとbを比較し大きい方の値を出力して下さい。aとbが同じ場合は「eq」と出力して下さい。
+# aとbを比較し大きい方の値を出力して下さい。aとbが同じ場合は「eq」と出力して下さい
 
+input = $stdin.gets.chomp.split
 
-a = gets.chomp.to_i 
-b = gets.chomp.to_i
-
-def num(a, b)
-  if a > b
-    puts a
-  elsif
-    a < b
-    puts b
-  else
-    puts "eq"
-  end
+if input[0] > input[1]
+  puts input[0]
+elsif input[0] < input[1]
+  puts input[1]
+else
+  puts "eq"
 end
-
-num(a, b)
