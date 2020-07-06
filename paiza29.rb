9370a50a1443fb3445898d99ac4e7a7a3e98d6cb
itokeso <61177741+itokeso@users.noreply.github.com>
2020-07-06 21:47:59 +0900

    paiza

---
+++ paiza29.rb
@@ -0,0 +1,14 @@
+# dあなたはあるワインの銘柄のマーケティングを担当しています。良いキャッチコピーを考えるため、とりあえず「〜の中で最高」というフレーズを色々作ってみることにしました。
+
+# 2 つの文字列が与えられるので、"Best in" とそれらの文字列をすべて半角スペース区切りで結合して出力してください。
+
+
+# 例)
+
+# hundred years → Best in hundred years
+
+# the universe → Best in the universe
+
+n = gets.to_s
+
+puts "Best in #{n}"
